f4ea0f9767e2749a4caa052ab8fc50adb3f6e70f
Cindano Jonathan <65620947+Snooker4Real@users.noreply.github.com>
2021-10-05 09:50:36 +0200

    adding menus

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const { app, BrowserWindow, ipcMain, dialog, Menu } = require("electron");
 const path = require("path");
 
 let homeWindow;
@@ -30,7 +30,7 @@ const profits = [
   },
 ];
 
-function createWindow(viewName, dataToSend, width=1400, height=1200) {
+function createWindow(viewName, dataToSend, width = 1400, height = 1200) {
   // Create the browser window
   const win = new BrowserWindow({
     width,
@@ -45,12 +45,13 @@ function createWindow(viewName, dataToSend, width=1400, height=1200) {
   });
 
   // and load the home.html of the app with some content (data sended to the fress view .then)
-  win.loadFile(path.join(__dirname, "views", viewName, viewName + ".html"))
-      .then(() => {
-        if (dataToSend){
-          win.send("init-data", dataToSend);
-        }
-  });
+  win
+    .loadFile(path.join(__dirname, "views", viewName, viewName + ".html"))
+    .then(() => {
+      if (dataToSend) {
+        win.send("init-data", dataToSend);
+      }
+    });
 
   // Only for debug Phase
   // Show all the time the dev tools
@@ -58,11 +59,10 @@ function createWindow(viewName, dataToSend, width=1400, height=1200) {
   return win;
 }
 
-app.whenReady()
-    .then(() => {
-      const data = {expenses, profits};
-      homeWindow = createWindow('home',data);
-    });
+app.whenReady().then(() => {
+  const data = { expenses, profits };
+  homeWindow = createWindow("home", data);
+});
 
 // Stuff for Mac
 app.on("window-all-closed", () => {
@@ -72,22 +72,22 @@ app.on("window-all-closed", () => {
 });
 app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    const data = {expenses, profits};
-    homeWindow = createWindow('home', data);
+    const data = { expenses, profits };
+    homeWindow = createWindow("home", data);
   }
 });
 
 ///////////////////////// New Item window listeners /////////////////////////////
-ipcMain.on('open-new-item-window',(e,data) =>{
 
-  if (newItemWindow){
+const openNewItemWindowCb = (e, data) => {
+  if (newItemWindow) {
     newItemWindow.focus();
     return;
   }
 
-  newItemWindow = createWindow('new-item',null,1000,500);
+  newItemWindow = createWindow("new-item", null, 1000, 500);
 
-  ipcMain.handle('new-item',(e,newItem) => {
+  ipcMain.handle("new-item", (e, newItem) => {
     // 5 steps:
 
     // newItem.id = selectedArray.length + 1;
@@ -100,10 +100,10 @@ ipcMain.on('open-new-item-window',(e,data) =>{
 
     // - Select the correct array to update
     // ternaire syntaxe => condition ? exprSiVrai : exprSiFaux
-    const selectedArray = data.type === 'profit' ? profits : expenses;
+    const selectedArray = data.type === "profit" ? profits : expenses;
 
     // - Create an id for the new item
-    if (selectedArray.length > 0){
+    if (selectedArray.length > 0) {
       // Select the last element of the array
       // Then select the id and add it 1
       id = selectedArray[selectedArray.length - 1].id + 1;
@@ -114,77 +114,76 @@ ipcMain.on('open-new-item-window',(e,data) =>{
     selectedArray.push(newItem);
 
     // - Send the array to the home view
-    homeWindow.send('new-item-added', {
+    homeWindow.send("new-item-added", {
       item: [newItem],
       type: data.type,
       expenses,
-      profits
-    })
+      profits,
+    });
 
     // - Send a response
-    return 'Item ajouté avec succès ✔️'
-  })
+    return "Item ajouté avec succès ✔️";
+  });
 
-  newItemWindow.on('closed', () => {
+  newItemWindow.on("closed", () => {
     newItemWindow = null;
-    ipcMain.removeHandler('new-item');
-  })
-});
+    ipcMain.removeHandler("new-item");
+  });
+};
 
-ipcMain.on('open-edit-item-window', (e,data) => {
-  if (editItemWindow){
+ipcMain.on("open-new-item-window", openNewItemWindowCb);
+
+ipcMain.on("open-edit-item-window", (e, data) => {
+  if (editItemWindow) {
     editItemWindow.close();
   }
 
-  const selectedTab = data.type === 'expenses' ? expenses : profits;
+  const selectedTab = data.type === "expenses" ? expenses : profits;
 
-  for (let [index,item] of selectedTab.entries()){
-    if (item.id === data.id){
+  for (let [index, item] of selectedTab.entries()) {
+    if (item.id === data.id) {
       // Permet de supprimer un certain nombre d'élément
       // à partir d'un index donné
-      editItemWindow = createWindow('edit-item',{item}, 1000,500);
+      editItemWindow = createWindow("edit-item", { item }, 1000, 500);
       // Slice permet d'extraire une partie d'un tableau
-      ipcMain.handle('edit-item',(e,data) =>{
-
+      ipcMain.handle("edit-item", (e, data) => {
         // Update
         selectedTab[index].label = data.label;
         selectedTab[index].value = data.value;
 
-        homeWindow.send('edited-item',{
+        homeWindow.send("edited-item", {
           item: selectedTab[index],
           expenses,
-          profits
-        })
+          profits,
+        });
 
-        return 'Item modifié avec succès ✔️✔️';
-
+        return "Item modifié avec succès ✔️✔️";
       });
       break;
     }
   }
-  editItemWindow.on('close',() => {
+  editItemWindow.on("close", () => {
     editItemWindow = null;
     // ipcMain.removeHandler('new-item');
   });
-})
+});
 
 ///////////////////////// Delete window listeners /////////////////////////////
-ipcMain.handle('show-confirm-delete-item', (e,data)=>{
-
+ipcMain.handle("show-confirm-delete-item", (e, data) => {
   const choice = dialog.showMessageBoxSync({
-    type: 'warning',
-    buttons: ['Non', 'Oui'],
-    title: 'Confirmation de suppression',
-    message: 'Êtes-vous sûr de vouloir supprimer l\'élément ?'
-  })
-  if (choice){
-    const selectedTab = data.type === 'expenses' ? expenses : profits;
+    type: "warning",
+    buttons: ["Non", "Oui"],
+    title: "Confirmation de suppression",
+    message: "Êtes-vous sûr de vouloir supprimer l'élément ?",
+  });
+  if (choice) {
+    const selectedTab = data.type === "expenses" ? expenses : profits;
 
-    for (let [index, item] of selectedTab.entries()){
-      if (item.id === data.id){
+    for (let [index, item] of selectedTab.entries()) {
+      if (item.id === data.id) {
         // Permet de supprimer un certain nombre d'élément
         // à partir d'un index donné
-        selectedTab.splice(index,1);
+        selectedTab.splice(index, 1);
         // Slice permet d'extraire une partie d'un tableau
         break;
       }
@@ -194,5 +193,50 @@ ipcMain.handle('show-confirm-delete-item', (e,data)=>{
     //
     // }
   }
-  return {choice, expenses, profits};
-})
+  return { choice, expenses, profits };
+});
+
+///////////////////// MENU CONFIG /////////////////////
+const menuConfig = [
+  {
+    label: "Action",
+    submenu: [
+      {
+        label: "Nouvelle dépense",
+        accelerator: "CmdOrCtrl+N",
+        click() {
+          openNewItemWindowCb(null, { type: "expense" });
+        },
+      },
+      {
+        label: "Nouvelle recette",
+        accelerator: "CmdOrCtrl+J",
+        click() {
+          openNewItemWindowCb(null, { type: "profit" });
+        },
+      },
+      {
+        label: "Activer/Désactiver le mode édition",
+        accelerator: "CmdOrCtrl+E",
+        click() {
+          homeWindow.send("toggle-edition-mode");
+        },
+      },
+    ],
+  },
+  {
+    label: "Fenêtre",
+    submenu: [
+      { role: "reload" },
+      { role: "toggledevtools" },
+      { type: "separator" },
+      { role: "togglefullscreen" },
+      { role: "minimize" },
+      { type: "separator" },
+      { role: "close" },
+    ],
+  },
+];
+
+const menu = Menu.buildFromTemplate(menuConfig); // Transforme le menu en objet
+Menu.setApplicationMenu(menu); // Apply the menu to the app
